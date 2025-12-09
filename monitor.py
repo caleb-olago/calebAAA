3fab422996537b753061ebb9e8dfd3f5d35e6ad6
caleb-olago <caleb.olago@laplateforme.io>
2025-12-09 21:19:42 +0100

    Ajout de la fonction infosystem pour afficher les informations sur le CPU

--- monitor.py
+++ monitor.py
@@ -1,2 +1,23 @@
 import psutil
 import time  
+import socket
+import platform
+import distro
+import datetime
+
+def infosystem():
+    while True:
+
+        print("\033c", end="")
+        
+        cpu_cores = psutil.cpu_count(logical=True)
+        cpu_frequency = psutil.cpu_freq().current
+        cpu_usage = psutil.cpu_percent(interval=1)
+
+        print()
+        print(" Informations sur le  CPU")
+        print()
+        print(f"Nombre de cœurs    : {cpu_cores}")
+        print(f"Fréquence actuelle : {cpu_frequency:.2f} MHz")
+        print(f"Utilisation CPU    : {cpu_usage:.1f} %")
+
