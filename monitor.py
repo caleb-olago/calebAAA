430a974bf891aba6cd6fdff544332ad64e455304
caleb-olago <caleb.olago@laplateforme.io>
2025-12-09 21:21:22 +0100

    Ajout des informations sur la RAM et les utilisateurs dans la fonction infosystem

--- monitor.py
+++ monitor.py
@@ -21,3 +21,25 @@ def infosystem():
         print(f"Fréquence actuelle : {cpu_frequency:.2f} MHz")
         print(f"Utilisation CPU    : {cpu_usage:.1f} %")
 
+        mem = psutil.virtual_memory()
+        usedram = mem.used / (1024**3)
+        allram = mem.total / (1024**3)
+        percentram = mem.percent
+
+        print()
+        print(" Informations sur la RAM ")
+        print()
+        print(f"RAM utilisée   : {usedram:.2f} GB")
+        print(f"RAM totale     : {allram:.2f} GB")
+        print(f"Utilisation RAM : {percentram:.1f} %")
+
+        hotsname = socket.gethostname()
+        system = platform.system()
+        if system == "Linux":
+            distrobution = distro.name()
+        else:
+            distrobution = "inconnue"
+
+        bootime_datetime = datetime.datetime.fromtimestamp(psutil.boot_time())
+        Nb_users = psutil.users()
+        ip = socket.gethostbyname(hotsname)
